70fb0e4bbc29805c1ce392f2cffb897d613d0f34
tn01ms <tn01ms@gmail.com>
2018-11-24 19:53:09 +0100

    Finished aufgabe 15

--- src/aufgabe15/Scan.java
+++ src/aufgabe15/Scan.java
@@ -2,19 +2,35 @@ package aufgabe15;
 
 import java.util.Arrays;
 
+/**
+ * Die Klasse Scan stellt eine Methode scan bereit die auf alle vorangehenden
+ * Stellen in einem Array a eine übergebene Operation anwendet und diese an der
+ * Stelle im Array speichert
+ * 
+ * @author Tobias
+ *
+ */
 public class Scan {
 
 	public static final int[] a = { 42, 17, 56, 98, 76, 33, 24, 15, 15, 29 };
 
+	/**
+	 * Scan ersetzt alle Elemente in a durch das Ergebnis der in op angegebenen
+	 * Operation von allen vorangestellten Arrayindizies
+	 * 
+	 * @param a  Das zu bearbeitende Array
+	 * @param op Die Operation
+	 * @return Der bearbeitete Array
+	 */
 	public static int[] scan(int[] a, IntOperation op) {
 		int[] erg = new int[a.length];
 
 		erg[0] = a[0];
 
 		for (int b = 1; b < a.length; b++) {
-			int temp = 0;
+			int temp = a[0];
 
-			for (int i = 0; i <= b; i++) {
+			for (int i = 1; i <= b; i++) {
 				temp = op.evaluate(temp, a[i]);
 			}
 
@@ -24,18 +40,41 @@ public class Scan {
 		return erg;
 	}
 
+	/**
+	 * Jede Stelle x in a wird durch die Summe der Stellen 0 bis x ersetzt
+	 * 
+	 * @param a
+	 * @return
+	 */
 	public static int[] scanAdd(int[] a) {
-		return scan(a, (i,y) -> (i + y));
+		return scan(a, (i, y) -> (i + y));
 	}
 
+	/**
+	 * Jede Stelle x in a wird durch die Subtraktion der Stellen 0 bis x ersetzt
+	 * 
+	 * @param a
+	 * @return
+	 */
 	public static int[] scanMul(int[] a) {
-		return scan(a, (i, y) -> (i * y)); //TODO, NOT WORKING YET
+		return scan(a, (i, y) -> (i * y));
 	}
 
+	/**
+	 * Jede Stelle x in a wird durch das Maximum der Stellen 0 bis x ersetzt
+	 * 
+	 * @param a
+	 * @return
+	 */
 	public static int[] scanMax(int[] a) {
 		return scan(a, (i, y) -> (Math.max(i, y)));
 	}
 
+	/**
+	 * Gibt die Ergebnisse des Tests von scanAdd, scanMul und scanMax aus.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int[] aAdd = scanAdd(a);
 		int[] aMul = scanMul(a);
