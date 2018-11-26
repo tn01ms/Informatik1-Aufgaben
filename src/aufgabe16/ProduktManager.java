a18f81045d5b7d7aa8e483c8b74c6ccc60281cf3
tn01ms <tn01ms@gmail.com>
2018-11-26 20:01:58 +0100

    Finished aufgabe 16

--- src/aufgabe16/ProduktManager.java
+++ src/aufgabe16/ProduktManager.java
@@ -3,17 +3,48 @@ package aufgabe16;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * Die Klasse Produktmanager stellt zwei Methoden bereit die zur Analyse von
+ * Mengen von Produkten geeignet sind
+ * 
+ * @author Tobias
+ *
+ */
 public class ProduktManager {
 
+	/**
+	 * Zählt alle Produkte im Stream mit mehr als 50KG Gewicht
+	 * 
+	 * @param p Der Produkt-Stream
+	 * @return Anzahl der schweren Produkte
+	 */
 	public long getAnzahlSchwereProdukte(Stream<Produkt> p) {
-		return 0;
-		// TODO
+		return p.filter(i -> i.getGewichtInGramm() >= 50000).count();
 	}
-	
+
+	/**
+	 * Errechnet ob der LKW lkw genug Platz und Gewichtskapazitäten bietet ist um
+	 * alle Produkte der Liste products zu verstauen
+	 * 
+	 * @param lkw      Der LKW
+	 * @param products Liste der Produkte
+	 * @return true -> LKW kann Produkte aufnehmen | false -> LKW kann Produkte
+	 *         nicht aufnehmen
+	 */
 	public boolean passtAufLKW(LKW lkw, List<Produkt> products) {
+		// Stream öffnen
 		Stream<Produkt> p = products.parallelStream();
-		// TODO
-		return false;
+		// KG summieren
+		int curKG = p.mapToInt(i -> i.getGewichtInGramm()).sum();
+		// Stream erneut öffnen
+		p = products.parallelStream();
+		// Volumen summieren
+		double curVol = p.mapToDouble(i -> i.getVolumenCM3()).sum();
+
+		// Maximalwerte
+		int maxKG = lkw.getMaxGewichtInKG();
+		double maxVol = lkw.getMaxM3Volumen();
+
+		return (curKG <= maxKG && curVol <= maxVol);
 	}
-	
 }
